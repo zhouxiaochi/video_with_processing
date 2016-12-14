022b2a3f00dab1166b454d88411d438c9fe5a6a1
zhouxiaochi <jimmyzhou.ntu@gmail.com>
2016-12-14 22:26:34 +0800

    Update and rename video_playground.pde to video_playground.java

---
+++ video_playground.java
@@ -0,0 +1,35 @@
+  
+import processing.video.*;
+
+Capture cam;
+
+void setup() {
+  size(displayWidth, displayHeight);
+
+  String[] cameras = Capture.list();
+  
+  if (cameras.length == 0) {
+    println("There are no cameras available for capture.");
+    exit();
+  } else {
+    println("Available cameras:");
+    for (int i = 0; i < cameras.length; i++) {
+      println(cameras[i]);
+    }
+    
+    // The camera can be initialized directly using an 
+    // element from the array returned by list():
+    cam = new Capture(this, cameras[0]);
+    cam.start();     
+  }      
+}
+
+void draw() {
+  if (cam.available() == true) {
+    cam.read();
+  }
+  tint(0,200,255);
+  image(cam, 0, 0);
+  print(cam);
+   
+}
